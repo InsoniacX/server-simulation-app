252d6b8b3e96187c9574c58da51dfd4979e568e6
InsoniacX <riandaarsyad@gmail.com>
2024-08-29 22:21:39 +0700

    "Remove deploy.yml workflow file"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0 0 0 0.4 5)',
+        '4xl': '0 45px 45px rgba(0 0 0 0.45)',
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+      backgroundImage: {
+        'hero-background': "url('/src/assets/bg-image.jpg')",
+      }
+    },
   },
   plugins: [],
 }
-
